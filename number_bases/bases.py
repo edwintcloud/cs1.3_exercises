1251c13fe8aad18625638ddb48513c56389941c3
Edwin Cloud <ecloud412@gmail.com>
2019-04-03 09:26:41 -0700

    :rocket: Initial Commit

---
+++ number_bases/bases.py
@@ -0,0 +1,92 @@
+#!python
+
+import string
+# Hint: Use these string constants to encode/decode hexadecimal digits and more
+# string.digits is '0123456789'
+# string.hexdigits is '0123456789abcdefABCDEF'
+# string.ascii_lowercase is 'abcdefghijklmnopqrstuvwxyz'
+# string.ascii_uppercase is 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# string.ascii_letters is ascii_lowercase + ascii_uppercase
+# string.printable is digits + ascii_letters + punctuation + whitespace
+
+def decode(digits, base):
+    """Decode given digits in given base to number in base 10.
+    digits: str -- string representation of number (in given base)
+    base: int -- base of given number
+    return: int -- integer representation of number (in base 10)"""
+    # Handle up to base 36 [0-9a-z]
+    assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
+    
+    # map chars [0-9a-z] to numbers [0-36]
+    num_map = {char: num for num, char in enumerate(string.digits + string.ascii_lowercase)}
+
+    # create a variable to hold result and multiplier
+    result, multiplier = 0, 1
+
+    for digit in digits[::-1]: 
+        result += num_map[digit.lower()] * multiplier
+        multiplier *= base # representative of base ^ ith power
+
+    # return the result
+    return result
+        
+
+
+def encode(number, base):
+    """Encode given number in base 10 to digits in given base.
+    number: int -- integer representation of number (in base 10)
+    base: int -- base to convert to
+    return: str -- string representation of number (in given base)"""
+    # Handle up to base 36 [0-9a-z]
+    assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
+    # Handle unsigned numbers only for now
+    assert number >= 0, 'number is negative: {}'.format(number)
+    
+    # map numbers [0-36] to chars [0-9a-z]
+    char_map = {num: char for num, char in enumerate(string.digits + string.ascii_lowercase)}
+
+    # create variable to hold resulting sequence
+    result = ""
+
+    # quotient will be used to divide by the base until we reach 0
+    quotient = number
+
+    while quotient > 0:
+        quotient, remainder = divmod(quotient, base)
+        result=char_map[int(remainder)] + result
+
+    # return result
+    return result
+
+def convert(digits, base1, base2):
+    """Convert given digits in base1 to digits in base2.
+    digits: str -- string representation of number (in base1)
+    base1: int -- base of given number
+    base2: int -- base to convert to
+    return: str -- string representation of number (in base2)"""
+    # Handle up to base 36 [0-9a-z]
+    assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
+    assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
+
+    # convert base1 to base 10 then from base 10 to base 2
+    return encode(decode(digits, base1), base2)
+
+
+def main():
+    """Read command-line arguments and convert given digits between bases."""
+    import sys
+    args = sys.argv[1:]  # Ignore script file name
+    if len(args) == 3:
+        digits = args[0]
+        base1 = int(args[1])
+        base2 = int(args[2])
+        # Convert given digits between bases
+        result = convert(digits, base1, base2)
+        print('{} in base {} is {} in base {}'.format(digits, base1, result, base2))
+    else:
+        print('Usage: {} digits base1 base2'.format(sys.argv[0]))
+        print('Converts digits from base1 to base2')
+
+
+if __name__ == '__main__':
+    main()
