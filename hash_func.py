5cfeccb07c85778ee5ab499f0a392258a60a82ed
Edwin Cloud <ecloud412@gmail.com>
2019-04-18 14:55:18 -0700

    Add hash table solutions

---
+++ hash_func.py
@@ -0,0 +1,14 @@
+def hash_str(string):
+    """hash_str uses the djb2 algorithm to compute the hash
+       value of a string http://www.cse.yorku.ca/~oz/hash.html"""
+    hash = 5381
+    for char in string[1:]:
+        # (hash << 5) + hash is equivalent to hash * 33
+        hash = (hash << 5) + hash + ord(char)
+    return hash
+
+
+# test
+result = hash_str("test")
+print(result)
+print(hash("test"))
